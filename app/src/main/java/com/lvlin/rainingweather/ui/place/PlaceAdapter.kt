3d18cdd0a9cb8134186876dedd5efb388c2e8624
lvlin <lin2.lv@tcl.com>
2021-10-25 17:57:41 +0800

    V1.2 新增切换城市和手动更新天气的功能

--- app/src/main/java/com/lvlin/rainingweather/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/lvlin/rainingweather/ui/place/PlaceAdapter.kt
@@ -22,23 +22,32 @@ class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: L
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.place_item, parent, false)
         val holder = ViewHolder(view)
-        holder.itemView.setOnClickListener{
+        holder.itemView.setOnClickListener {
             val position = holder.adapterPosition
             val place = placeList[position]
-            val intent = Intent(parent.context, WeatherActivity::class.java).apply {
-                putExtra("location_lng", place.location.lng)
-                putExtra("location_lat", place.location.lat)
-                putExtra("place_name", place.name)
+            val activity = fragment.activity
+            if (activity is WeatherActivity) {
+                activity.drawerLayout.closeDrawers()
+                activity.viewModel.locationLng = place.location.lng
+                activity.viewModel.locationLat = place.location.lat
+                activity.viewModel.placeName = place.name
+                activity.refreshWeather()
+            } else {
+                val intent = Intent(parent.context, WeatherActivity::class.java).apply {
+                    putExtra("location_lng", place.location.lng)
+                    putExtra("location_lat", place.location.lat)
+                    putExtra("place_name", place.name)
+                }
+                fragment.startActivity(intent)
+                fragment.activity?.finish()
             }
             fragment.viewModel.savePlace(place)
-            fragment.startActivity(intent)
-            fragment.activity?.finish()
         }
         return holder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-       val place = placeList[position]
+        val place = placeList[position]
         holder.placeName.text = place.name
         holder.placeAdress.text = place.address
     }
--- app/src/main/java/com/lvlin/rainingweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/lvlin/rainingweather/ui/weather/WeatherActivity.kt
@@ -1,14 +1,19 @@
 package com.lvlin.rainingweather.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.lvlin.rainingweather.R
 import com.lvlin.rainingweather.logic.model.Weather
 import com.lvlin.rainingweather.logic.model.getSky
@@ -32,11 +37,16 @@ class WeatherActivity : AppCompatActivity() {
     private lateinit var carWashingText: TextView
     private lateinit var weatherLayout: ScrollView
 
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var navButton: Button
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (Build.VERSION.SDK_INT >= 21) {
             val decorView = window.decorView
-            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
             window.statusBarColor = Color.TRANSPARENT
         }
         setContentView(R.layout.activity_weather)
@@ -53,6 +63,14 @@ class WeatherActivity : AppCompatActivity() {
         weatherLayout = findViewById(R.id.weatherLayout)
 
         forecastLayout = findViewById(R.id.forecastLayout)
+
+        swipeRefresh = findViewById(R.id.swipeRefresh)
+        navButton = findViewById(R.id.navButton)
+        drawerLayout = findViewById(R.id.drawerLayout)
+
+
+
+
         if (viewModel.locationLng.isEmpty()) {
             viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
         }
@@ -70,8 +88,42 @@ class WeatherActivity : AppCompatActivity() {
                 Toast.makeText(this, "获取天气信息失败", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
+            swipeRefresh.isRefreshing = false
         })
+        swipeRefresh.setColorSchemeResources(R.color.design_default_color_primary)
+        refreshWeather()
+        swipeRefresh.setOnRefreshListener {
+            refreshWeather()
+        }
+
+        navButton.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerStateChanged(newState: Int) {
+            }
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+            }
+
+            override fun onDrawerClosed(drawerView: View) {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE)
+                        as InputMethodManager
+                manager.hideSoftInputFromWindow(
+                    drawerView.windowToken,
+                    InputMethodManager.HIDE_NOT_ALWAYS
+                )
+            }
+        })
+    }
+
+    fun refreshWeather() {
         viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+        swipeRefresh.isRefreshing = true
     }
 
     private fun showWeatherInfo(weather: Weather) {
